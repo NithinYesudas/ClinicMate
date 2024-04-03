769dc6df798fc438ab9d1c6e73b7ac1d9714f210
Abhinand-R <abhinand62003@gmail.com>
2024-04-04 00:59:39 +0530

    3

--- mini_pro_main/lib/screens/signin_screen.dart
+++ mini_pro_main/lib/screens/signin_screen.dart
@@ -3,6 +3,8 @@ import 'package:flutter/src/widgets/container.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:mini_pro_main/utils/color_utils.dart';
 
+import '../reusable_widgets/reusable_widget.dart';
+
 class SignInScreen extends StatefulWidget {
   const SignInScreen({super.key});
 
@@ -11,6 +13,9 @@ class SignInScreen extends StatefulWidget {
 }
 
 class _SignInScreenState extends State<SignInScreen> {
+  TextEditingController _passwordTextController = TextEditingController();
+  TextEditingController _emailTextController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,26 +27,28 @@ class _SignInScreenState extends State<SignInScreen> {
           hexStringToColor("5E61F4")
         ], begin: Alignment.topCenter, end: Alignment.bottomCenter)),
         child: SingleChildScrollView(
-            child: Padding(
-                padding: EdgeInsets.fromLTRB(
-                    20, MediaQuery.of(context).size.height * 0.2, 20, 0),
-                child: Column(
-                  children: <Widget>[
-                    logoWidget(
-                        "assets/images/logosthestscope_heart000627-removebg-preview.png")
-                  ],
-                ))),
+          child: Padding(
+            padding: EdgeInsets.fromLTRB(
+                20, MediaQuery.of(context).size.height * 0.2, 20, 0),
+            child: Column(
+              children: <Widget>[
+                logoWidget(
+                    "assets/images/logosthestscope_heart000627-removebg-preview.png"),
+                SizedBox(
+                  height: 30,
+                ),
+                resuableTextfield("Enter username", Icons.person_outline, false,
+                    _emailTextController),
+                SizedBox(
+                  height: 30,
+                ),
+                resuableTextfield("Enter passwoerdd", Icons.lock_outline, true,
+                    _emailTextController),
+              ],
+            ),
+          ),
+        ),
       ),
-    );
-  }
-
-  Image logoWidget(String imageName) {
-    return Image.asset(
-      imageName,
-      fit: BoxFit.fitWidth,
-      width: 240,
-      height: 240,
-      color: Colors.white,
     );
   }
 }
