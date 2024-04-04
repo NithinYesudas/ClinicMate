d1feb09ac3c07a5c901f20cf05dd16bb8f43fb83
Abhinand-R <abhinand62003@gmail.com>
2024-04-05 02:51:28 +0530

    frontendcomplete

---
+++ mini_pro_main/lib/screens/home_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:mini_pro_main/screens/signin_screen.dart';
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          child: Text("Logout"),
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => SignInScreen()));
+          },
+        ),
+      ),
+    );
+  }
+}
