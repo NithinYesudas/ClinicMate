b6235b7967b2268cd225b7bb415eb13ac75356b1
Abhinand-R <abhinand62003@gmail.com>
2024-05-02 00:06:29 +0530

    apointment firebstore

--- mini_pro_main/lib/models/appointment_booking.dart
+++ mini_pro_main/lib/models/appointment_booking.dart
@@ -1,23 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:mini_pro_main/models/doctors.dart';
 import 'package:mini_pro_main/screens/home_screen.dart';
 import 'package:table_calendar/table_calendar.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'timeslotpg.dart';
 
-void main() {
-  runApp(MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Doctor Appointment',
-      home: DoctorCard(),
-    );
-  }
-}
-
 class DoctorCard extends StatefulWidget {
+  final Doctor doctor;
+
+  DoctorCard({required this.doctor});
+
   @override
   _DoctorCardState createState() => _DoctorCardState();
 }
@@ -26,6 +18,28 @@ class _DoctorCardState extends State<DoctorCard> {
   CalendarFormat _calendarFormat = CalendarFormat.month;
   DateTime _focusedDay = DateTime.now();
   DateTime? _selectedDay;
+  Map<String, dynamic>? _doctorInfo;
+
+  @override
+  void initState() {
+    super.initState();
+    _fetchDoctorInfo();
+  }
+
+  Future<void> _fetchDoctorInfo() async {
+    try {
+      final snapshot = await FirebaseFirestore.instance
+          .collection('doctors')
+          .where('name', isEqualTo: widget.doctor.name)
+          .get();
+      final data = snapshot.docs.first.data();
+      setState(() {
+        _doctorInfo = data;
+      });
+    } catch (e) {
+      // Handle any exceptions
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,10 +48,7 @@ class _DoctorCardState extends State<DoctorCard> {
         leading: IconButton(
           icon: Icon(Icons.arrow_back),
           onPressed: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const HomeScreen()),
-            ); // Handle back button press
+            Navigator.pop(context);
           },
         ),
         actions: [
@@ -49,92 +60,98 @@ class _DoctorCardState extends State<DoctorCard> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16.0),
-            child: CircleAvatar(
-              radius: 50.0,
-              backgroundImage: AssetImage('assets/doctor_image.jpg'),
-            ),
-          ),
-          Text(
-            'Cardiologist',
-            style: TextStyle(
-              fontSize: 20.0,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            'Dr. Alan C Braverman',
-            style: TextStyle(
-              fontSize: 18.0,
-            ),
-          ),
-          SizedBox(height: 16.0),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: Icon(Icons.location_on),
-                onPressed: () {
-                  // Handle location button press
-                },
-              ),
-            ],
-          ),
-          SizedBox(height: 16.0),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Text(
-              'Dr. Alan C. Braverman is a cardiologist in Saint Louis, Missouri and is affiliated with multiple hospitals in the area.',
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-          ),
-          SizedBox(height: 16.0),
-          TableCalendar(
-            firstDay: DateTime.utc(2023, 1, 1),
-            lastDay: DateTime.utc(2024, 12, 31),
-            focusedDay: _focusedDay,
-            calendarFormat: _calendarFormat,
-            selectedDayPredicate: (day) {
-              return isSameDay(_selectedDay, day);
-            },
-            onDaySelected: (selectedDay, focusedDay) {
-              setState(() {
-                _selectedDay = selectedDay;
-                _focusedDay = focusedDay;
-              });
-            },
-            onFormatChanged: (format) {
-              setState(() {
-                _calendarFormat = format;
-              });
-            },
-            onPageChanged: (focusedDay) {
-              _focusedDay = focusedDay;
-            },
-          ),
-          SizedBox(height: 16.0),
-          ElevatedButton(
-            onPressed: () {
-              if (_selectedDay != null) {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) =>
-                        TimeSlotPage(selectedDate: _selectedDay!),
+      body: _doctorInfo == null
+          ? Center(child: CircularProgressIndicator())
+          : SingleChildScrollView(
+              // Wrap with SingleChildScrollView
+              child: Column(
+                children: [
+                  Padding(
+                    padding: EdgeInsets.all(16.0),
+                    child: CircleAvatar(
+                      radius: 50.0,
+                      backgroundImage:
+                          NetworkImage(_doctorInfo!['imageUrl'] ?? ''),
+                    ),
                   ),
-                );
-              }
-            },
-            child: const Text('Book Appointment'),
-          ),
-        ],
-      ),
+                  Text(
+                    _doctorInfo!['speciality'] ?? '',
+                    style: TextStyle(
+                      fontSize: 20.0,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 8.0),
+                  Text(
+                    _doctorInfo!['name'],
+                    style: TextStyle(
+                      fontSize: 18.0,
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      IconButton(
+                        icon: Icon(Icons.location_on),
+                        onPressed: () {
+                          // Handle location button press
+                        },
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 16.0),
+                  Padding(
+                    padding: EdgeInsets.symmetric(horizontal: 16.0),
+                    child: Text(
+                      _doctorInfo!['about'],
+                      style: TextStyle(
+                        fontSize: 16.0,
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: 16.0),
+                  TableCalendar(
+                    firstDay: DateTime.utc(2023, 1, 1),
+                    lastDay: DateTime.utc(2024, 12, 31),
+                    focusedDay: _focusedDay,
+                    calendarFormat: _calendarFormat,
+                    selectedDayPredicate: (day) {
+                      return isSameDay(_selectedDay, day);
+                    },
+                    onDaySelected: (selectedDay, focusedDay) {
+                      setState(() {
+                        _selectedDay = selectedDay;
+                        _focusedDay = focusedDay;
+                      });
+                    },
+                    onFormatChanged: (format) {
+                      setState(() {
+                        _calendarFormat = format;
+                      });
+                    },
+                    onPageChanged: (focusedDay) {
+                      _focusedDay = focusedDay;
+                    },
+                  ),
+                  SizedBox(height: 16.0),
+                  ElevatedButton(
+                    onPressed: () {
+                      if (_selectedDay != null) {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) =>
+                                TimeSlotPage(selectedDate: _selectedDay!),
+                          ),
+                        );
+                      }
+                    },
+                    child: const Text('Book Appointment'),
+                  ),
+                ],
+              ),
+            ),
     );
   }
 }
--- mini_pro_main/lib/screens/home_screen.dart
+++ mini_pro_main/lib/screens/home_screen.dart
@@ -213,7 +213,7 @@ class _HomeScreenState extends State<HomeScreen> {
         Navigator.push(
           context,
           MaterialPageRoute(
-            builder: (context) => DoctorCard(),
+            builder: (context) => DoctorCard(doctor: doctor),
           ),
         );
       },
